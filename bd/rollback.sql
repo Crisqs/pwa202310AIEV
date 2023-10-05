cdb54a8fd8de608c49ff35a8d9b8f3bd2d63da7c
crisqs <crisqqsolis@gmail.com>
2023-10-05 14:08:15 -0600

    Se hicieron modificaciones en la carpeta BD

--- bd/rollback.sql
+++ bd/rollback.sql
@@ -2,3 +2,14 @@ use utsotur;
 
 TRUNCATE TABLE contacto;
 DROP TABLE  contacto;
+
+TRUNCATE TABLE recorrido;
+DROP TABLE  recorrido;
+
+TRUNCATE TABLE sitio_informaivo;
+DROP TABLE  sitio_informaivo;
+
+TRUNCATE TABLE usuario;
+DROP TABLE  usuario;
+
+drop DATABASE utsotur
